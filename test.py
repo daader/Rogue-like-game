e783afc2eb766bba172459a7bebd5d41021ad848
Mask <110684512+daader@users.noreply.github.com>
2023-12-03 01:30:59 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,52 +1,43 @@
-
 import pygame 
-import sys
 
 pygame.init() 
 
 screen_hight = 800
 screen_width = screen_hight*0.8
-pygame.display.set_caption("Moving Rectangle")
+screen_color = (255,255,255) 
 
-position_x = 400
-position_y = position_x*0.8
+screen = pygame.display.set_mode((screen_hight, screen_width))
 
-rect_higth = 30
-rect_width = 30
+player_x = 400
+player_y = player_x*0.8
+player_higth = 30
+player_width = 30
+player_color = (255,0,0) 
+player_speed = 1
 
-color = (255,255,255) 
-rect_color = (255,0,0) 
-rect_speed = 1
-rect = pygame.Rect(position_x,position_y,rect_higth,rect_width)
-screen = pygame.display.set_mode((rect_width,rect_higth))
+player = pygame.Rect(player_x, player_y, player_higth, player_width)
 
-# CREATING CANVAS 
-canvas = pygame.display.set_mode((screen_hight, screen_width)) 
-  
-# TITLE OF CANVAS 
-pygame.display.set_caption("My Board")  
-  
+
 exit = False
-  
+
 while not exit: 
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             exit = True
-            sys.exit()
 
     keys = pygame.key.get_pressed()
-    # Check for arrow key inputs
+
     if keys[pygame.K_LEFT]:
-        rect.x -= rect_speed
+        player.x -= player_speed
     if keys[pygame.K_RIGHT]:
-        rect.x += rect_speed
+        player.x += player_speed
     if keys[pygame.K_UP]:
-        rect.y -= rect_speed
+        player.y -= player_speed
     if keys[pygame.K_DOWN]:
-        rect.y += rect_speed
+        player.y += player_speed
 
-    screen.fill('black')
+    screen.fill(screen_color)
 
-    pygame.draw.rect(canvas, rect_color, rect) 
+    pygame.draw.rect(screen, player_color, player) 
 
     pygame.display.update()
